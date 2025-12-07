6db8be2058dabb5537e5a7b29fb9742d743da733
Andrey <kagamlykas@gmail.com>
2025-12-07 19:29:57 +0200

    Solved task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,33 @@
 'use strict';
 
-// write your code here
+const numbersSpan = [...document.querySelectorAll('.population')];
+
+/* eslint-disable function-paren-newline */
+const numbers = numbersSpan.map((i) =>
+  parseInt(i.innerText.replaceAll(',', '')),
+);
+
+const sum = numbers.reduce((prev, item) => prev + item, 0);
+const avg = Math.round(sum / numbers.length);
+
+const total = document.querySelector('.total-population');
+const average = document.querySelector('.average-population');
+
+total.innerText = toStringWithSeparator(sum);
+average.innerText = toStringWithSeparator(avg);
+
+function toStringWithSeparator(number) {
+  const numberOfString = number.toString().split('');
+  const stringLength = numberOfString.length;
+  let count =
+    stringLength % 3 === 0
+      ? stringLength / 3 - 1
+      : Math.floor(stringLength / 3);
+
+  for (let index = stringLength - 3; count > 0; count--) {
+    numberOfString.splice(index, 0, ',');
+    index -= 3;
+  }
+
+  return numberOfString.join('');
+}
