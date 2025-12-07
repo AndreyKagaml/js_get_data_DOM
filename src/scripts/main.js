b1d3cd2ceb1331ba7c3bd791061950ceee619a70
Andrey <kagamlykas@gmail.com>
2025-12-07 19:59:04 +0200

    Solved task by feedback format

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,10 +4,9 @@ const numbersSpan = [...document.querySelectorAll('.population')];
 
 /* eslint-disable function-paren-newline */
 if (!checkIsEmpty(numbersSpan)) {
-
   const numbers = numbersSpan
     .map((i) => parseInt(i.innerText.replaceAll(',', ''), 10))
-    .filter(i => i);
+    .filter((i) => i);
 
   if (!checkIsEmpty(numbers)) {
     const sum = numbers.reduce((prev, item) => prev + item, 0);
@@ -16,29 +15,22 @@ if (!checkIsEmpty(numbersSpan)) {
     const total = document.querySelector('.total-population');
     const average = document.querySelector('.average-population');
 
-    if (total) total.innerText = toStringWithSeparator(sum);
-    if (total) average.innerText = toStringWithSeparator(avg);
+    if (total) {
+      total.innerText = toStringWithSeparator(sum);
+    }
+
+    if (total) {
+      average.innerText = toStringWithSeparator(avg);
+    }
   }
 }
 
 function toStringWithSeparator(number) {
-  const numberOfString = number.toString().split('');
-  const stringLength = numberOfString.length;
-  let count =
-    stringLength % 3 === 0
-      ? stringLength / 3 - 1
-      : Math.floor(stringLength / 3);
-
-  for (let index = stringLength - 3; count > 0; count--) {
-    numberOfString.splice(index, 0, ',');
-    index -= 3;
-  }
-
-  return numberOfString.join('');
+  return new Intl.NumberFormat('en-US').format(number);
 }
 
-function checkIsEmpty(array){
-  if (array === null){
+function checkIsEmpty(array) {
+  if (array === null) {
     return true;
   }
 
